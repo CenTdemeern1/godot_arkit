e1ccda75aef12cfcf2a85a5af3f30180e2f5518e
CenTdemeern1 <timo.herngreen@gmail.com>
2026-03-02 14:36:57 +0100

    Tweak autoload.gd to fix some issues
    Incorrect capitalization, also the incomplete file path was making it fail to load the singleton instance

--- addons/godot_arkit/autoload.gd
+++ addons/godot_arkit/autoload.gd
@@ -1,16 +1,15 @@
 @tool
 extends Node
 
-
-var ArKitInterfaceSingleton_instance
+var ARKitInterfaceSingleton_instance
 
 func _enter_tree():
 	if OS.get_name() == "iOS":
-		var ArKitInterfaceSingleton_class = load("ARKitInterfaceSingleton.gd")
-		ArKitInterfaceSingleton_instance = ArKitInterfaceSingleton_class.new()
-		add_child(ArKitInterfaceSingleton_instance)
+		var ARKitInterfaceSingleton_class = load("res://addons/godot_arkit/ARKitInterfaceSingleton.gd")
+		ARKitInterfaceSingleton_instance = ARKitInterfaceSingleton_class.new()
+		add_child(ARKitInterfaceSingleton_instance)
 
 func _exit_tree():
 	if OS.get_name() == "iOS":
-		if ArKitInterfaceSingleton_instance:
-			ArKitInterfaceSingleton_instance.queue_free()
+		if ARKitInterfaceSingleton_instance:
+			ARKitInterfaceSingleton_instance.queue_free()
